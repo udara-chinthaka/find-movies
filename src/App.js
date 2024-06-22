3d7b49a6e35abcf62e1cb42d1b7081254555b9c4
Udara Chinthaka <udarachinthaka135@gmail.com>
2024-06-22 21:39:32 +0300

    adding S

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import searchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
-const API_URL = "http://www.omdbapi.com?apikey=76c1ccda";
+const API_URL = "https://www.omdbapi.com?apikey=76c1ccda";
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
